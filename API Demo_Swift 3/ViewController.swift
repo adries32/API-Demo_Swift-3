dc7b130b63a079f48dd44ad58ea314ef0f981b43
Mhofu <jtmhembere@yahoo.com>
2016-09-08 20:43:44 -0500

    Initial Commit

---
+++ API Demo_Swift 3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  API Demo_Swift 3
+//
+//  Created by johannes Mhembere on 9/8/16.
+//  Copyright © 2016 mhofu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
